1f12e50492c47fffa2485f5ee473cb311263904a
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2022-09-21 15:01:43 -0300

    feat: add attribute cd_phone_number and AUTO_INCREMENT to cd_registration.

--- db_registrations.sql
+++ db_registrations.sql
@@ -5,10 +5,10 @@ CREATE DATABASE IF NOT EXISTS db_registrations;
 USE db_registrations;
 
 CREATE TABLE tb_registrations (
-cd_registration INT NOT NULL,
+cd_registration INT NOT NULL AUTO_INCREMENT,
 nm_person VARCHAR(200) NOT NULL,
 cd_cpf CHAR(11) NOT NULL UNIQUE,
-cd_telefone VARCHAR(11) NOT NULL,
+cd_phone_number VARCHAR(11) NOT NULL,
 CONSTRAINT pk_registrations
 	PRIMARY KEY(cd_registration)
 );
