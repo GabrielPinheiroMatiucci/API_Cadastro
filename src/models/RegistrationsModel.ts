8f3131a0180bcfa06843efeabaedd64de9072e09
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2022-09-21 14:34:46 -0300

    feat: add model.

---
+++ src/models/RegistrationsModel.ts
@@ -0,0 +1,46 @@
+import connection from './connection';
+import IRegistration from '../interfaces/IRegistration';
+
+class RegistrationsModel {
+  private connection;
+
+  constructor (conn = connection) {
+    this.connection = conn;
+  }
+
+  public async getRegistrations() {
+    try {
+      const registrations = await this.connection.execute(
+        'SELECT * FROM tb_registrations;',
+      );
+  
+      return registrations[0];
+    } catch (err) {      
+      return null;
+    }
+  }
+
+  public async createRegistration(registration: IRegistration) {
+    try {
+      const thereIsCpf: any = await this.connection.execute(
+        'SELECT cd_registration FROM tb_registrations WHERE cd_cpf = ?;',
+        [registration.cpf]
+      );
+
+      if (thereIsCpf[0].length > 0) {
+        return 'CPF já cadastrado';
+      } else {
+        const response: any = await this.connection.execute(
+          'INSERT INTO tb_registrations (nm_person, cd_cpf) VALUES (?, ?);',
+          [registration.name, registration.cpf],
+        );
+
+        return response[0].affectedRows === 1 ? true : null;
+      }
+    } catch (err) {      
+      return null;      
+    }
+  }
+}
+
+export default RegistrationsModel;
---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+import RegistrationsModel from './RegistrationsModel';
+import connection from './connection';
+
+export {
+  RegistrationsModel,
+  connection,
+};
